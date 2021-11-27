ab94e6e5908a5dfc534895d85726a382246ad13e
Shane <marusczaks@gmail.com>
2021-11-27 12:41:48 -0500

    refactored to take in words as toml files

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 pub mod to {
     use std::collections::HashMap;
 
+    const MAX_STRING_DISTANCE: i32 = 1;
+
     ///This function takes an integer and returns its English name.
     ///
     /// ```
@@ -14,50 +16,16 @@ pub mod to {
     ///
     /// assert_eq!(to::string(-42), "negative forty two");
     ///
-    /// assert_eq!(to::int(&to::string(42)).expect(""), 42);
+    /// assert_eq!(to::int(&to::string(42)).unwrap(), 42);
     ///
-    /// assert_eq!(to::string(to::int("forty two").expect("")), "forty two");
+    /// assert_eq!(to::string(to::int("forty two").unwrap()), "forty two");
     /// ```
     pub fn string(num: isize) -> String {
         let mut num_internal = num;
         if num_internal == 0 {
             return String::from("zero");
         }
-        let units = [
-            "",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "ten",
-            "eleven",
-            "twelve",
-            "thirteen",
-            "fourteen",
-            "fifteen",
-            "sixteen",
-            "seventeen",
-            "eighteen",
-            "nineteen",
-        ];
-
-        let tens = [
-            "", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
-        ];
-
-        let scales = [
-            "",
-            "thousand",
-            "million",
-            "billion",
-            "trillion",
-            "quadrillion",
-        ];
+        let word_lists: &StringWordLists = &load_string_words_list();
 
         let mut is_neg = false;
         if num_internal < 0 {
@@ -65,11 +33,11 @@ pub mod to {
             is_neg = true;
         }
         let mut current_scale = 0;
-        let mut words: String = String::from("");
+        let mut words: String = String::new();
         while num_internal > 0 {
             if num_internal % 1000 != 0 {
-                words = to_string_helper(num_internal % 1000, &units, &tens)
-                    + scales[current_scale]
+                words = to_string_helper(num_internal % 1000, word_lists)
+                    + &word_lists.scales[current_scale]
                     + " "
                     + &words[..];
             }
@@ -82,25 +50,27 @@ pub mod to {
         String::from(words.trim())
     }
 
-    fn to_string_helper(num: isize, units: &[&str], tens: &[&str]) -> String {
+    fn to_string_helper(num: isize, word_lists: &StringWordLists) -> String {
         if num == 0 {
             String::from("")
         } else if num < 20 {
-            String::from(units[num as usize]) + " "
+            String::from(&word_lists.units[num as usize]) + " "
         } else if num < 100 {
-            String::from(tens[(num / 10) as usize])
+            String::from(&word_lists.tens[(num / 10) as usize])
                 + " "
-                + to_string_helper(num % 10, &units, &tens).as_str()
+                + to_string_helper(num % 10, word_lists).as_str()
         } else {
-            String::from(units[(num / 100) as usize])
+            String::from(&word_lists.units[(num / 100) as usize])
                 + " hundred "
-                + to_string_helper(num % 100, &units, &tens).as_str()
+                + to_string_helper(num % 100, word_lists).as_str()
         }
     }
 
     ///This function takes an English name of an integer and returns its integer value as an isize.
     ///
     /// Will still convert for input errors a distance of 1 from the correct spelling of the English name.
+    ///
+    /// Will offer suggestion on possible word if input error is greater than a distance of 1 from the correct spelling of the English name.
     ///
     /// Returns
     /// Ok(isize),
@@ -109,65 +79,40 @@ pub mod to {
     /// ```
     /// # use to_int_and_back::to;
     ///
-    /// assert_eq!(to::int("forty two").expect(""), 42);
+    /// assert_eq!(to::int("forty two").unwrap(), 42);
     ///
-    /// assert_eq!(to::int("negative forty two").expect(""), -42);
+    /// assert_eq!(to::int("negative forty two").unwrap(), -42);
     ///
-    /// assert_eq!(to::int("frty twoo").expect(""), 42);
+    /// assert_eq!(to::int("frty twoo").unwrap(), 42);
     ///
-    ///```
-    /// ```should_panic
-    /// # use to_int_and_back::to;
-    ///
-    /// assert_eq!(to::int("fty twwoo").expect(""), 42); //panics!
+    /// assert_eq!(to::int("fty two").unwrap_err(), "Did you mean forty?");
     ///```
     pub fn int(text_num: &str) -> Result<isize, String> {
         let text_num_inner = &text_num.to_lowercase()[..];
-        let mut num_words: HashMap<&str, (isize, isize)> = HashMap::new();
-        let units = [
-            "zero",
-            "one",
-            "two",
-            "three",
-            "four",
-            "five",
-            "six",
-            "seven",
-            "eight",
-            "nine",
-            "ten",
-            "eleven",
-            "twelve",
-            "thirteen",
-            "fourteen",
-            "fifteen",
-            "sixteen",
-            "seventeen",
-            "eighteen",
-            "nineteen",
-        ];
-        let tens = [
-            "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
-        ];
-        let scales = [
-            "hundred",
-            "thousand",
-            "million",
-            "billion",
-            "trillion",
-            "quadrillion",
-        ];
-        num_words.insert("and", (1, 0));
 
-        for (index, word) in units.iter().enumerate() {
-            num_words.insert(*word, (1, index as isize));
+        let mut num_words: HashMap<&str, (isize, isize)> = HashMap::new();
+
+        let word_lists: &IntWordLists = &load_int_words_list();
+
+        let mut all_words: Vec<&str> = Vec::with_capacity(
+            word_lists.units.len() + word_lists.tens.len() + word_lists.scales.len() + 1,
+        );
+
+        num_words.insert("and", (1, 0));
+        all_words.push("and");
+
+        for (index, word) in word_lists.units.iter().enumerate() {
+            num_words.insert(word, (1, index as isize));
+            all_words.push(word);
         }
-        for (index, word) in tens.iter().enumerate() {
-            num_words.insert(*word, (1, index as isize * 10));
+        for (index, word) in word_lists.tens.iter().enumerate() {
+            num_words.insert(word, (1, index as isize * 10));
+            all_words.push(word);
         }
         let num: usize = 10;
-        for (index, word) in scales.iter().enumerate() {
-            num_words.insert(*word, (num.pow(get_power(index)) as isize, 0));
+        for (index, word) in word_lists.scales.iter().enumerate() {
+            num_words.insert(word, (num.pow(get_power(index)) as isize, 0));
+            all_words.push(word);
         }
         let mut current = 0;
         let mut result = 0;
@@ -181,15 +126,14 @@ pub mod to {
                     return Err(String::from("Invalid input"));
                 }
             }
-            let word_next = if !num_words.contains_key(&word) {
-                find_possible_matches(&word, &num_words)
+            let word_next = if !all_words.contains(&word) {
+                match find_matching_word(&word, &all_words) {
+                    Ok(word) => word,
+                    Err(e) => return Err(e),
+                }
             } else {
                 String::from(word)
             };
-
-            if word_next.contains("Did you mean") {
-                return Err(word_next);
-            }
 
             let (scale, increment) = num_words[word_next.as_str()];
             current = current * scale + increment;
@@ -208,24 +152,24 @@ pub mod to {
         }
     }
 
-    fn find_possible_matches(word: &str, words: &HashMap<&str, (isize, isize)>) -> String {
+    fn find_matching_word(word: &str, words: &Vec<&str>) -> Result<String, String> {
         let mut min_dist = 9999;
-        let mut final_string: String = String::from("");
-        for (w, _) in words {
+        let mut final_string = String::new();
+        for w in words {
             let distance = min_distance(w, word);
             if distance < min_dist {
                 min_dist = distance;
                 final_string = String::from(*w);
             }
         }
-        if min_dist > 1 {
+        if min_dist > MAX_STRING_DISTANCE {
             if min_distance(word, "negative") < 5 {
-                return String::from("Did you mean negative?");
+                return Err(String::from("Did you mean negative?"));
             } else {
-                return String::from(format!("Did you mean {}?", final_string));
+                return Err(String::from(format!("Did you mean {}?", final_string)));
             }
         }
-        final_string
+        Ok(final_string)
     }
 
     fn min_distance(word1: &str, word2: &str) -> i32 {
@@ -249,79 +193,138 @@ pub mod to {
         }
         dist[word2.len()] as i32
     }
-}
 
-#[cfg(test)]
-mod tests {
-    use super::to;
-    #[test]
-    fn tests_should_not_panic_to_int() {
-        assert_eq!(to::int("One Hundred And Forty Two").expect(""), 142);
-        assert_eq!(
-            to::int(
-                "one million four hundred twenty seven thousand four hundred and seventy three"
-            )
-            .expect(""),
-            1_427_473
-        );
-        assert_eq!(
-            to::int("negative seven thousand three hundred and ninety six").expect(""),
-            -7396
-        );
-        assert_eq!(
-            to::int("negativ three hundre and fifty fiv").expect(""),
-            -355
-        );
-        assert_eq!(
-            to::int(&to::string(-123_456_789_098_765_432)).expect(""),
-            -123_456_789_098_765_432
-        );
+    use serde::Deserialize;
+
+    use std::fs::File;
+    use std::io::Read;
+    use std::path::Path;
+
+    #[derive(Deserialize)]
+    struct StringWordLists {
+        units: Vec<String>,
+        tens: Vec<String>,
+        scales: Vec<String>,
     }
 
-    #[test]
-    fn tests_should_not_panic_to_string() {
-        assert_eq!(to::string(142), "one hundred forty two");
-        assert_eq!(
-            to::string(1_427_473),
-            "one million four hundred twenty seven thousand four hundred seventy three"
-        );
-        assert_eq!(
-            to::string(-7396),
-            "negative seven thousand three hundred ninety six"
-        );
-        assert_eq!(to::string(-355), "negative three hundred fifty five");
-        assert_eq!(
-            to::string(
-                to::int("negative twenty seven thousand eight hundred sixty nine").expect("")
-            ),
-            "negative twenty seven thousand eight hundred sixty nine"
-        );
+    #[derive(Deserialize)]
+    struct IntWordLists {
+        units: Vec<String>,
+        tens: Vec<String>,
+        scales: Vec<String>,
     }
 
-    #[test]
-    #[should_panic(expected = "Did you mean hundred?")]
-    fn tests_should_panic_message_1() {
-        assert_eq!(to::int("one hured and forty two").expect(""), 142);
+    fn load_string_words_list() -> StringWordLists {
+        let path = Path::new("static/string_words.toml");
+        let display = path.display();
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+        let mut file_content = String::new();
+        file.read_to_string(&mut file_content).unwrap();
+        let test: StringWordLists = toml::from_str(&file_content).unwrap();
+        test
     }
 
-    #[test]
-    #[should_panic(expected = "Did you mean negative?")]
-    fn tests_should_panic_message_2() {
-        assert_eq!(to::int("ngtiv one hundred forty two").expect(""), 142);
+    fn load_int_words_list() -> IntWordLists {
+        let path = Path::new("static/int_words.toml");
+        let display = path.display();
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+        let mut file_content = String::new();
+        file.read_to_string(&mut file_content).unwrap();
+        let test: IntWordLists = toml::from_str(&file_content).unwrap();
+        test
     }
 
-    #[test]
-    #[should_panic(expected = "Invalid input")]
-    fn tests_should_panic_message_3() {
-        assert_eq!(
-            to::int("negative negative one hundred forty two").expect(""),
-            142
-        );
-    }
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        #[test]
+        fn tests_should_not_panic_to_int() {
+            assert_eq!(int("One Hundred And Forty Two").unwrap(), 142);
+            assert_eq!(
+                int(
+                    "one million four hundred twenty seven thousand four hundred and seventy three"
+                )
+                .unwrap(),
+                1_427_473
+            );
+            assert_eq!(
+                int("negative seven thousand three hundred and ninety six").unwrap(),
+                -7396
+            );
+            assert_eq!(int("negativ three hundre and fifty fiv").unwrap(), -355);
+            assert_eq!(
+                int(&string(-123_456_789_098_765_432)).unwrap(),
+                -123_456_789_098_765_432
+            );
+        }
 
-    #[test]
-    #[should_panic(expected = "Invalid input")]
-    fn tests_should_panic_message_4() {
-        assert_eq!(to::int("ten negative").expect(""), -10);
+        #[test]
+        fn tests_should_not_panic_to_string() {
+            assert_eq!(string(142), "one hundred forty two");
+            assert_eq!(
+                string(1_427_473),
+                "one million four hundred twenty seven thousand four hundred seventy three"
+            );
+            assert_eq!(
+                string(-7396),
+                "negative seven thousand three hundred ninety six"
+            );
+            assert_eq!(string(-355), "negative three hundred fifty five");
+            assert_eq!(
+                string(int("negative twenty seven thousand eight hundred sixty nine").unwrap()),
+                "negative twenty seven thousand eight hundred sixty nine"
+            );
+        }
+
+        #[test]
+        fn min_distance_tests() {
+            assert_eq!(min_distance("big fish", "big wish"), 1);
+            assert_eq!(min_distance("negative", "negativ"), 1);
+            assert_eq!(min_distance("negative", "ngtiv"), 3);
+            assert_eq!(min_distance("hello", "goodbye"), 7);
+        }
+
+        #[test]
+        fn find_matching_word_tests() {
+            let words = vec!["gourami", "tetra", "snail"];
+            assert_eq!(find_matching_word("gorami", &words).unwrap(), "gourami");
+            assert_eq!(
+                find_matching_word("gormi", &words).unwrap_err(),
+                "Did you mean gourami?"
+            );
+            assert_eq!(find_matching_word("teta", &words).unwrap(), "tetra");
+            assert_eq!(
+                find_matching_word("tet", &words).unwrap_err(),
+                "Did you mean tetra?"
+            );
+            assert_eq!(find_matching_word("sail", &words).unwrap(), "snail");
+            assert_eq!(
+                find_matching_word("sal", &words).unwrap_err(),
+                "Did you mean snail?"
+            );
+        }
+
+        #[test]
+        fn test_error_messages() {
+            assert_eq!(
+                int("one hured and forty two").unwrap_err(),
+                "Did you mean hundred?"
+            );
+            assert_eq!(
+                int("ngtiv one hundred forty two").unwrap_err(),
+                "Did you mean negative?"
+            );
+            assert_eq!(
+                int("negative negative one hundred forty two").unwrap_err(),
+                "Invalid input"
+            );
+            assert_eq!(int("ten negative").unwrap_err(), "Invalid input");
+        }
     }
 }
